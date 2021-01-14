84b55fdd3e956e8c201585ea7360acc30c3981fa
shaneocon <shane.pat.oconnell@gmail.com>
2021-01-13 17:29:21 -0800

    added util

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
