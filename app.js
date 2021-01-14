bb8ffbbf6b834aa5866391a1c7d25ac1fadeb978
shaneocon <shane.pat.oconnell@gmail.com>
2021-01-13 18:39:11 -0800

    added addManager function

--- app.js
+++ app.js
@@ -4,13 +4,44 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const util = require("util");
-const writeFileAsync = util.promisify(fs.writeFile);
+
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+
+const teamArr = [];
+
+function addManager() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the Manager's name?",
+            name: "managerName"
+        },
+        {
+            type: "input",
+            message: "What is the Manager's ID number?",
+            name: "managerID"
+        },
+        {
+            type: "input",
+            message: "What is the Manager's email address?",
+            name: "managerEmail"
+        },
+        {
+            type: "input",
+            message: "What is the Manager's office number?",
+            name: "managerOffice"
+        }
+    ]).then(response => {
+        const newManager = new Manager(response.managerName, response.managerID, response.managerEmail, response.managerOffice);
+        teamList.push(newManager);
+        chooseMember();
+    });
+}
+
 
 
 // Write code to use inquirer to gather information about the development team members,
